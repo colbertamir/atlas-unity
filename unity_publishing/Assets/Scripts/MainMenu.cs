3297d0e47d5ddb361f5b2c50685ba1329fb17e8b
colbertamir <colbertamir1516@gmail.com>
2024-02-15 09:46:24 -0600

    ok please work

---
+++ unity_publishing/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Initialization
+    void Start()
+    {
+
+    }
+
+    // Update called once per frame
+    void Update()
+    {
+
+    }
+
+    // Method to play the maze scene
+    public void PlayMaze()
+    {
+        // Load the maze scene
+        SceneManager.LoadScene("maze");
+    }
+
+    // Method to quit the game
+    public void QuitMaze()
+    {
+#if UNITY_EDITOR
+        Debug.Log("Quit Game"); // Logs message when running in Unity editor
+#else
+        Application.Quit(); // Quits game when running outside of Unity editor
+#endif
+    }
+}
