c7cfa04795b6d76f76f2a0ec294fbf16db3ca264
colbertamir <colbertamir1516@gmail.com>
2024-03-24 22:58:37 -0500

    compilation errors fixed

--- unity-animation/Assets/Scripts/MainMenu.cs
+++ unity-animation/Assets/Scripts/MainMenu.cs
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// Method to load the selected level scene
 public class MainMenu : MonoBehaviour
 {
     // Method to load the selected level scene
@@ -39,45 +39,3 @@ public class MainMenu : MonoBehaviour
         Application.Quit(); // Quits game
     }
 }
-=======
-using System.Collections;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class MainMenu : MonoBehaviour
-{
-    // Method to load the selected level scene
-    public void LevelSelect(int level)
-    {
-        // Check which level button was clicked and load the corresponding scene
-        switch (level)
-        {
-            case 1:
-                SceneManager.LoadScene("Level01"); // Load Level01
-                break;
-            case 2:
-                SceneManager.LoadScene("Level02"); // Load Level02
-                break;
-            case 3:
-                SceneManager.LoadScene("Level03"); // Load Level03
-                break;
-            default:
-                Debug.LogError("Invalid level selection!"); // Log error if an invalid level is selected
-                break;
-        }
-    }
-
-    // Method to load the Options scene
-    public void Options()
-    {
-        SceneManager.LoadScene("Options"); // Load Options
-    }
-
-    // Method to exit the game
-    public void ExitGame()
-    {
-        Debug.Log("Exited"); // Logs "Exited" to Console
-        Application.Quit(); // Quits game
-    }
-}
->>>>>>> 1eed6a1f476dc9bbb52b675e027801dc5eef8052
